dc8cfd0c731ec1bc481f007ea6ba74a8f5eb1e8e
Breno Sobral <ssobralbreno@gmail.com>
2024-07-04 11:54:57 -0400

    feat: add maker array

--- src/dashboard/application/use-cases/get-guilds.use-case.ts
+++ src/dashboard/application/use-cases/get-guilds.use-case.ts
@@ -23,14 +23,34 @@ export class GetGuildsUseCase {
         'Master Sorcerer': 'Sorcerer',
       };
 
-      return members.reduce((acc, member) => {
-        const vocationGroup = vocationMap[member.vocation] || member.vocation;
+      const grouped = members.reduce((acc, member) => {
+        const vocationGroup =
+          member.level < 200
+            ? 'MAKER'
+            : vocationMap[member.vocation] || member.vocation;
         if (!acc[vocationGroup]) {
           acc[vocationGroup] = [];
         }
         acc[vocationGroup].push(member);
         return acc;
       }, {});
+
+      for (const key in grouped) {
+        grouped[key].sort((a, b) => b.level - a.level);
+      }
+
+      const result = {};
+      Object.keys(grouped)
+        .sort((a, b) => {
+          if (a === 'MAKER') return 1;
+          if (b === 'MAKER') return -1;
+          return 0;
+        })
+        .forEach((key) => {
+          result[key] = grouped[key];
+        });
+
+      return result;
     };
 
     const groupedOnlineMembers = groupByVocation(onlineMembers);
