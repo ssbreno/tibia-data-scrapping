721a01eb4dbc98a365e356a110ea8bd2b33d7d17
Breno Sobral <ssobralbreno@gmail.com>
2024-07-06 01:36:10 -0400

    feat: add monitor

--- src/dashboard/infraestructure/database/prisma/repositories/respawn.prisma.repository.ts
+++ src/dashboard/infraestructure/database/prisma/repositories/respawn.prisma.repository.ts
@@ -29,16 +29,24 @@ export class PrismaRespawnRepository implements RespawnRepository {
   async update(name: string, data: Partial<Respawn>): Promise<Respawn> {
     const existingRespawn = await this.prisma.respawn.findFirst({
       where: {
-        character: { equals: data.character },
+        character: { equals: name },
       },
     });
 
     if (existingRespawn) {
       const updatedRespawn = await this.prisma.respawn.update({
-        where: { character: data.character },
+        where: { character: name },
         data,
       });
       return updatedRespawn;
+    } else {
+      const newRespawn = await this.prisma.respawn.create({
+        data: {
+          ...data,
+          character: name,
+        },
+      });
+      return newRespawn;
     }
   }
 
